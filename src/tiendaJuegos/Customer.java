5ac24343b5c4499560e510f82c205451954f9e50
ferrakkt <ferrakkt@gmail.com>
2026-09-16 17:30:41 +0200

    Clases creadas (correción)

--- src/tiendaJuegos/Customer.java
+++ src/tiendaJuegos/Customer.java
@@ -1,5 +1,8 @@
 package tiendaJuegos;
 
 public class Customer {
-
+	 private int id;
+	 private String name;
+	 private double balance;
+	 
 }
--- src/tiendaJuegos/Main.java
+++ src/tiendaJuegos/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Pon el pdf en el aula, que asi lo veo lentamente porque no me acuerdo nada del año pasado. "
+				+ "Gracias :D");
 	}
 
 }
